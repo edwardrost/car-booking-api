b75f08ca23bfc73fa582982aa4678cb51adae39d
edwardrost <edward.rost@gmail.com>
2020-01-23 15:51:03 +0300

    added cloud mongodb with mongoose connect

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const carRoutes = require('./routes/car')
 const orderRoutes = require('./routes/order')
+const keys = require('./config/keys')
 
 const app = express()
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true,  useUnifiedTopology: true} )
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.log(error))
+
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded( {extended: true} ))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ed-dev:er020903@cluster0-qvic1.mongodb.net/test?retryWrites=true&w=majority'
+}
